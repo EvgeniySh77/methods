8ae90c1cd3c645b5f0f133b5367ebdc1b8312cec
EvgeniySh77 <evgen.sh77@mail.ru>
2022-01-24 18:40:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main()
+        {             
+            (string name, string[] dishes) User;
+
+            Console.Write("Введите свое имя: ");
+            User.name = Console.ReadLine();
+
+            User.dishes = new string[5];
+            for (int i = 0; i < User.dishes.Length; i++)
+            {
+                Console.Write($"Введите свое любимое блюдо номер {i + 1}: ");
+                User.dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
